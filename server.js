a29751c49cd242411a945dcdfab8d2e74409d629
Bill Newman <william.newman@seattlecolleges.edu>
2025-05-13 23:08:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = 3000;
 //Create server, deal with requests/responses
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
+
+  //default HTML
+  res.statusCode = 200;
+  res.end('<h1>Welcome to Node.js!</h1><p>Please add interesting info here.</p>');
+
 });
 
 //Set server to listen for requests
